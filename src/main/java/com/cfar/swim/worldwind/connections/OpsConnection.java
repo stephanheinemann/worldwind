861ae3ac7a275a68a8e4d533c9564fb7db8ad3bf
Stephan Heinemann <stephan@rigiresearch.com>
2017-02-28 11:09:20 -0800

    #11: added operations connection

---
+++ src/main/java/com/cfar/swim/worldwind/connections/OpsConnection.java
@@ -0,0 +1,23 @@
+package com.cfar.swim.worldwind.connections;
+
+public abstract class OpsConnection implements Connection {
+
+	@Override
+	public void connect() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disconnect() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
