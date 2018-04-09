154a99754f8c2c7c0f86781e4e4118e5f3c41b79
ManuelDCR <manueldiogorosa@gmail.com>
2018-04-08 17:41:21 -0700

    #13 started the implementation of an RRT planner

---
+++ src/main/java/com/cfar/swim/worldwind/ai/continuum/basicrrt/RRTreePlanner.java
@@ -0,0 +1,266 @@
+/**
+ * Copyright (c) 2018, Manuel Rosa (UVic Center for Aerospace Research)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.cfar.swim.worldwind.ai.continuum.basicrrt;
+
+import java.time.ZonedDateTime;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import com.cfar.swim.worldwind.ai.continuum.AbstractSampler;
+import com.cfar.swim.worldwind.aircraft.Aircraft;
+import com.cfar.swim.worldwind.planning.Environment;
+import com.cfar.swim.worldwind.planning.Trajectory;
+import com.cfar.swim.worldwind.planning.Waypoint;
+
+import gov.nasa.worldwind.geom.Position;
+
+/**
+ * Realizes a basic RRT planner that plans a trajectory of an aircraft in an
+ * environment considering a local cost and risk policy. The origin of the tree
+ * is the departure position and it is grown until the goal is reached.
+ * 
+ * @author Manuel Rosa
+ *
+ */
+public class RRTreePlanner extends AbstractSampler {
+
+    /** the maximum number of sampling iterations */
+    static final int MAX_ITER = 1000_000;
+
+    // ---------- VARIABLES ----------
+
+    /** the start RRT waypoint */
+    private RRTreeWaypoint start = null;
+
+    /** the goal RRT waypoint */
+    private RRTreeWaypoint goal = null;
+
+    /** the set of sampled RRT waypoints */
+    protected Set<RRTreeWaypoint> tree = new HashSet<>();
+
+    /** the last computed plan */
+    private final LinkedList<RRTreeWaypoint> plan = new LinkedList<>();
+
+    // ---------- CONSTRUCTORS ----------
+
+    public RRTreePlanner(Aircraft aircraft, Environment environment) {
+	super(aircraft, environment);
+	// TODO Define how the constructor for the planner should be implemented
+    }
+
+    /**
+     * Creates an RRT waypoint at a specified position.
+     * 
+     * @param position the position
+     * 
+     * @return the RRT waypoint at the specified position
+     */
+    protected RRTreeWaypoint createWaypoint(Position position) {
+	return new RRTreeWaypoint(position);
+    }
+
+    // ---------- Setters and Getters ----------
+
+    /**
+     * Gets the start RRT waypoint of this RRT planner.
+     * 
+     * @return the start RRT waypoint of this RRT planner
+     */
+    public RRTreeWaypoint getStart() {
+	return start;
+    }
+
+    /**
+     * Sets the start RRT waypoint of this RRT planner.
+     * 
+     * @param start the start waypoint of this RRT planner
+     */
+    public void setStart(RRTreeWaypoint start) {
+	this.start = start;
+    }
+
+    /**
+     * Gets the goal RRT waypoint of this RRT planner.
+     * 
+     * @return the goal RRT waypoint of this RRT planner
+     */
+    public RRTreeWaypoint getGoal() {
+	return goal;
+    }
+
+    /**
+     * Sets the goal RRT waypoint of this RRT planner.
+     * 
+     * @param goal the goal waypoint of this RRT planner
+     */
+    public void setGoal(RRTreeWaypoint goal) {
+	this.goal = goal;
+    }
+
+    // ---------- PROTECTED METHODS ----------
+
+    /**
+     * Creates a trajectory of the computed plan.
+     * 
+     * @return the trajectory of the computed plan
+     */
+    @SuppressWarnings("unchecked")
+    protected Trajectory createTrajectory() {
+	return new Trajectory((List<Waypoint>) this.plan.clone());
+    }
+
+    /**
+     * Samples a new waypoint sampled from a uniformed distribution over the
+     * environment space
+     * 
+     * @return waypoint the RRTreeWaypoint sampled
+     */
+    protected RRTreeWaypoint sampleRandom() {
+	// TODO: How to implement a random sampling of the environment
+
+	return null;
+    }
+
+    /**
+     * Samples a new waypoint from the environment with a given bias to the goal
+     * 
+     * @param bias the percentage value of bias to sample the goal
+     * 
+     * @return waypoint the RRTreeWaypoint sampled
+     */
+    protected RRTreeWaypoint sampleBiased(int bias) {
+	RRTreeWaypoint waypoint;
+	// TODO: Is there any problem in using the random package?
+	int rand = new Random().nextInt(100 - 1) + 1;
+
+	waypoint = (rand <= bias) ? this.getGoal() : this.sampleRandom();
+
+	return waypoint;
+    }
+
+    /**
+     * Extends the tree in the direction of the given waypoint and returns the
+     * status according to the result of the extension
+     * 
+     * @param waypoint the waypoint set as the goal for extension
+     * @return status the status resulting from the extend
+     */
+    protected Status extendRRT(RRTreeWaypoint waypoint) {
+	Status status = Status.TRAPPED;
+
+	return status;
+    }
+
+    /**
+     * Check whether the new waypoint is the goal or within the goal region
+     * 
+     * @return true if the new waypoint is within the goal region
+     */
+    protected boolean checkGoal() {
+	// TODO: Define how to compare if the new waypoint is the goal/in the
+	// goal region
+	return false;
+    }
+
+    /**
+     * Computes the backwards path from the current waypoint to the starting waypoint 
+     */
+    protected void computePath() {
+	// TODO: compute the path from the new waypoint to the start by
+	// repeatedly calling the parent of each node
+    }
+
+    /**
+     * Initializes the planner to plan from an origin to a destination at a
+     * specified estimated time of departure.
+     * 
+     * @param origin the origin in globe coordinates
+     * @param destination the destination in globe coordinates
+     * @param etd the estimated time of departure
+     */
+    protected void initialize(Position origin, Position destination,
+	    ZonedDateTime etd) {
+
+	this.setStart(this.createWaypoint(origin));
+	this.setGoal(this.createWaypoint(destination));
+
+    }
+
+    /**
+     * Computes a plan by growing a tree until the goal is reached
+     */
+    protected void compute() {
+	for (int i = 0; i < MAX_ITER; i++) {
+	    RRTreeWaypoint waypointRand = this.sampleBiased(5);
+	    if (this.extendRRT(waypointRand) != Status.TRAPPED) {
+		if (this.checkGoal()) {
+		    this.computePath();
+		    return;
+		}
+	    }
+	}
+    }
+
+    // ---------- PUBLIC METHODS ----------
+
+    @Override
+    public Trajectory plan(Position origin, Position destination,
+	    ZonedDateTime etd) {
+	// TODO Implement the planning strategy according to this algorithm 
+	
+	// this.initialize(origin, destination, etd);
+	// this.compute();
+	// Trajectory trajectory = this.createTrajectory();
+	// this.revisePlan(trajectory);
+	// return trajectory;
+
+	this.initialize(origin, destination, etd);
+	this.compute();
+	Trajectory trajectory = this.createTrajectory();
+	// this.revisePlan(trajectory);
+	// return trajectory;
+
+	return trajectory;
+    }
+
+    @Override
+    public Trajectory plan(Position origin, Position destination,
+	    List<Position> waypoints, ZonedDateTime etd) {
+	// TODO Implement a planning strategy to allow for intermidiate goals
+	
+	// Multiple calls to plan (origin, destination, etd) creating an entire
+	// tree between multiple goals
+	return null;
+    }
+
+}
