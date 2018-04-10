ddd550f175afd14ee27ac4a7ad6cfe8f2680fe53
henriquebferreira <hmbf1995@gmail.com>
2018-04-09 18:00:06 -0700

    #12 AbstractSampler

--- src/main/java/com/cfar/swim/worldwind/ai/continuum/AbstractSampler.java
+++ src/main/java/com/cfar/swim/worldwind/ai/continuum/AbstractSampler.java
@@ -49,113 +49,113 @@ import gov.nasa.worldwind.geom.Position;
  *
  */
 public abstract class AbstractSampler extends AbstractPlanner
-	implements Sampler {
+implements Sampler {
 
-    // TODO: Should this be defined here or specifically for every algorithm? (Probably not here...)
-    /** the list of already sampled waypoints */
-    private List<? extends Waypoint> waypointList = null;
+	// TODO: Should this be defined here or specifically for every algorithm? (Probably not here...)
+	/** the list of already sampled waypoints */
+	private List<? extends Waypoint> waypointList = null;
 
-    /**
-     * Constructs a sampling based motion planner with a specified aircraft and
-     * environment.
-     * 
-     * @param aircraft the aircraft for planning
-     * @param environment the environment for planning
-     */
-    public AbstractSampler(Aircraft aircraft, Environment environment) {
-	super(aircraft, environment);
-    }
-
-    /**
-     * Gets the list of already sampled waypoints
-     * 
-     * @return the waypointList
-     */
-    public List<? extends Waypoint> getWaypointList() {
-	return waypointList;
-    }
-
-    /**
-     * Sets the list of waypoints previously sampled
-     * 
-     * @param waypointList the waypointList to set
-     */
-    public void setWaypointList(List<? extends Waypoint> waypointList) {
-	this.waypointList = waypointList;
-    }
-
-    /**
-     * Samples a position from a continuous space defined in the current
-     * environment
-     * 
-     * @return position in global coordinates inside the environment
-     */
-    public Position sampleRandomPosition() {
-	Position position = new Position(null, null, 0);
-	
-	// TODO: implement random sampler of position coordinates
-	//     : based on environment borders
-	
-	return position;
-    }
-   
-    /**
-     * Checks if a given position is in conflict with untraversable obstacles in
-     * the environment
-     * 
-     * @param waypoint the waypoint in global coordinates
-     * 
-     * @return boolean value true if there is a conflict
-     */
-    public boolean checkConflict(Position waypoint) {
-	return false;
-    }
-
-    /**
-     * Checks if a straight leg between the waypoints is in conflict with
-     * untraversable obstacles in the environment
-     * 
-     * @param waypoint1 the first waypoint in global coordinates
-     * @param waypoint2 the second waypoint in global coordinates
-     * 
-     * @return boolean value true if there is a conflict
-     */
-    public boolean checkConflict(Position waypoint1, Position waypoint2) {
-	return false;
-    }
-    
-    /**
-     * Finds the k-nearest waypoints to the given waypoint considering the
-     * problems metric
-     * 
-     * @param waypoint the waypoint in global coordinates
-     * @param num number of waypoints to return
-     * 
-     * @return list of k-nearest waypoints sorted by increasing distance
-     */
-    public List<? extends Position> findNearest(Position waypoint, int num) {
-	List<Position> posiNearList = new ArrayList<Position>();
-	List<Position> posiTempList = new ArrayList<Position>(waypointList);
-	
-	// sorts the list by increasing distance to waypoint
-	Collections.sort(posiTempList,
-		(a, b) -> super.getEnvironment().getDistance(waypoint,
-			a) < super.getEnvironment().getDistance(waypoint, b)
-		? -1
-			: super.getEnvironment().getDistance(waypoint,
-				a) == super.getEnvironment()
-				.getDistance(waypoint, b) ? 0
-					: 1);
-	
-	// If there are less than k neighbors on the list
-	if (posiTempList.size() <= num) {
-	    posiNearList = posiTempList;
-	} else {
-	    for (int k = 0; k < num; k++) {
-		posiNearList.add(posiTempList.get(k));
-	    }
+	/**
+	 * Constructs a sampling based motion planner with a specified aircraft and
+	 * environment.
+	 * 
+	 * @param aircraft the aircraft for planning
+	 * @param environment the environment for planning
+	 */
+	public AbstractSampler(Aircraft aircraft, Environment environment) {
+		super(aircraft, environment);
 	}
-	
-	return posiNearList;
-    }
+
+	/**
+	 * Gets the list of already sampled waypoints
+	 * 
+	 * @return the waypointList
+	 */
+	public List<? extends Waypoint> getWaypointList() {
+		return waypointList;
+	}
+
+	/**
+	 * Sets the list of waypoints previously sampled
+	 * 
+	 * @param waypointList the waypointList to set
+	 */
+	public void setWaypointList(List<? extends Waypoint> waypointList) {
+		this.waypointList = waypointList;
+	}
+
+	/**
+	 * Samples a position from a continuous space defined in the current
+	 * environment
+	 * 
+	 * @return position in global coordinates inside the environment
+	 */
+	public Position sampleRandomPosition() {
+		Position position = new Position(null, null, 0);
+
+		// TODO: implement random sampler of position coordinates
+		//     : based on environment borders
+
+		return position;
+	}
+
+	/**
+	 * Checks if a given position is in conflict with untraversable obstacles in
+	 * the environment
+	 * 
+	 * @param waypoint the waypoint in global coordinates
+	 * 
+	 * @return boolean value true if there is a conflict
+	 */
+	public boolean checkConflict(Position waypoint) {
+		return false;
+	}
+
+	/**
+	 * Checks if a straight leg between the waypoints is in conflict with
+	 * untraversable obstacles in the environment
+	 * 
+	 * @param waypoint1 the first waypoint in global coordinates
+	 * @param waypoint2 the second waypoint in global coordinates
+	 * 
+	 * @return boolean value true if there is a conflict
+	 */
+	public boolean checkConflict(Position waypoint1, Position waypoint2) {
+		return false;
+	}
+
+	/**
+	 * Finds the k-nearest waypoints to the given waypoint considering the
+	 * problems metric
+	 * 
+	 * @param waypoint the waypoint in global coordinates
+	 * @param num number of waypoints to return
+	 * 
+	 * @return list of k-nearest waypoints sorted by increasing distance
+	 */
+	public List<? extends Position> findNearest(Position waypoint, int num) {
+		List<Position> posiNearList = new ArrayList<Position>();
+		List<Position> posiTempList = new ArrayList<Position>(waypointList);
+
+		// sorts the list by increasing distance to waypoint
+		Collections.sort(posiTempList,
+				(a, b) -> super.getEnvironment().getDistance(waypoint,
+						a) < super.getEnvironment().getDistance(waypoint, b)
+				? -1
+						: super.getEnvironment().getDistance(waypoint,
+								a) == super.getEnvironment()
+								.getDistance(waypoint, b) ? 0
+										: 1);
+
+		// If there are less than k neighbors on the list
+		if (posiTempList.size() <= num) {
+			posiNearList = posiTempList;
+		} else {
+			for (int k = 0; k < num; k++) {
+				posiNearList.add(posiTempList.get(k));
+			}
+		}
+
+		return posiNearList;
+	}
 }
