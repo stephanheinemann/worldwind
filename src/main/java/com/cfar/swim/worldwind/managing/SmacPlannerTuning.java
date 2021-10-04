46b070d6a2b10eaf786b12a37a09e38e1605deb9
Stephan Heinemann <stephan@rigiresearch.com>
2021-10-04 09:38:36 -0700

    #63: added missing SMAC files

--- src/main/java/com/cfar/swim/worldwind/managing/SmacPlannerTuning.java
+++ src/main/java/com/cfar/swim/worldwind/managing/SmacPlannerTuning.java
@@ -27,75 +27,59 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.cfar.swim.worldwind.managers;
+package com.cfar.swim.worldwind.managing;
 
-import com.cfar.swim.worldwind.managing.Features;
-import com.cfar.swim.worldwind.managing.PlannerTuning;
-import com.cfar.swim.worldwind.managing.SmacManagerMode;
-import com.cfar.swim.worldwind.managing.SmacPlannerTuning;
+import java.util.List;
+
 import com.cfar.swim.worldwind.planners.Planner;
+import com.cfar.swim.worldwind.registries.Properties;
 import com.cfar.swim.worldwind.registries.Specification;
-import com.cfar.swim.worldwind.util.Identifiable;
 
 /**
- * Realizes a sequential model-based algorithm configuration autonomic manager.
- *  
+ * Realizes a SMAC planner tuning based on features.
+ * 
  * @author Stephan Heinemann
  *
+ * @see PlannerTuning
  */
-public class SmacAutonomicManager extends AbstractAutonomicManager implements AutonomicManager {
+public class SmacPlannerTuning extends PlannerTuning {
 	
-	/** the manager mode of this SMAC autonomic manager */
-	private SmacManagerMode managerMode = SmacManagerMode.EXECUTING;
-	
-	/**
-	 * Gets the identifier of this SMAC autonomic manager.
-	 * 
-	 * @return the identifier of this SMAC autonomic manager
-	 * 
-	 * @see Identifiable#getId()
-	 */
-	@Override
-	public String getId() {
-		return Specification.MANAGER_SMAC_ID;
-	}
+	/** the default serial identification of this SMAC planner tuning */
+	private static final long serialVersionUID = 1L;
 	
 	/**
-	 * Gets the manager mode of this SMAC autonomic manager.
-	 * 
-	 * @return the manager mode of this SMAC autonomic manager
-	 */
-	public SmacManagerMode getManagerMode() {
-		return this.managerMode;
-	}
-	
-	/**
-	 * Sets the manager mode of this SMAC autonomic manager.
-	 * 
-	 * @param managerMode the manager mode to be set
-	 */
-	public void setManagerMode(SmacManagerMode managerMode) {
-		this.managerMode = managerMode;
-	}
-	
-	/**
-	 * Creates a new SMAC planner tuning for this SMAC autonomic manager based
-	 * on a planner specification and features.
+	 * Constructs a new SMAC planner tuning based on a planner
+	 * specification and features.
 	 * 
 	 * @param specification the planner specification
 	 * @param features the features
 	 * 
-	 * @return the created SMAC planner tuning
-	 * 
-	 * @see AbstractAutonomicManager#createPlannerTuning(Specification, Features)
+	 * @throws IllegalArgumentException if the planner specification or
+	 *         features are invalid
+	 *
+	 * @see PlannerTuning#PlannerTuning(Specification, Features)
 	 */
-	@Override
-	public PlannerTuning createPlannerTuning(
+	public SmacPlannerTuning(
 			Specification<Planner> specification, Features features) {
-		return new SmacPlannerTuning(specification, features);
+		super(specification, features);
 	}
 	
-	// TODO: override abstract manager methods for SmacManagerMode.TRAINING
-	// wrap SMAC extension with AbstractAlgorithmFramework.evaluateRun overridden
+	/**
+	 * Tunes the specification of a planner according to features.
+	 * 
+	 * @param specification the planner specification to be tuned
+	 * @param features the features to tune the planner specification for
+	 * 
+	 * @return the tuned candidate properties for the specification
+	 * 
+	 * @see FeatureTuning#tune(Specification, Features)
+	 */
+	@Override
+	public List<Properties<Planner>> tune(
+			Specification<Planner> specification, Features features) {
+		// TODO: only select configuration based on loaded model (warm-start)
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
